528bff261d344e8d23d7d5d594090278aad0e86a
Luka Miletic <l.miletic@msgnetconomy.net>
2020-02-05 09:16:29 +0100

    Posting and order

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControlls from '../../components/Burger/BuildControlls/BuildControlls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -67,7 +68,23 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        alert('You continue!');
+        const order = {
+            ingredients: this.state.ingredients,
+            totalPrice: this.state.totalPrice,
+            customer: {
+                name: 'Pera Peric',
+                address: {
+                    street: 'Perina Ulica',
+                    zipCode: '11000',
+                    country: 'Serbia'
+                },
+                email: 'pera@peric.com'
+            },
+            deliveryMethod: 'express'
+        };
+        axios.post('/orders.json', order)
+            .then(response => console.log(response))
+            .catch(error => console.log(error));
     }
 
     render () {
